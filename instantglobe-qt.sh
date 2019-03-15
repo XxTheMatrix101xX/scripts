8cc6be3a13f24a7ccbdf24290d0a5f8c57d4bf24
TheMatrix101 <33000692+XxTheMatrix101xX@users.noreply.github.com>
2019-03-14 20:05:44 -0500

    Update instantglobe-qt.sh
    added apt install git for OSes that don't have git preinstalled

--- instantglobe-qt.sh
+++ instantglobe-qt.sh
@@ -1,4 +1,4 @@
-sudo apt-get install -y build-essential libssl-dev libdb++-dev libboost-all-dev libqrencode-dev miniupnpc libminiupnpc-dev qt5-default qt5-qmake qtbase5-dev-tools qttools5-dev-tools  build-essential libboost-dev libboost-system-dev  libboost-filesystem-dev libboost-program-options-dev libboost-thread-dev libssl-dev libdb++-dev libminiupnpc-dev  qtscript5-dev qtscript-tools  libqt5webkit5 libqt5webkit5-dev libqt5webkit5-qmlwebkitplugin
+sudo apt-get install -y git build-essential libssl-dev libdb++-dev libboost-all-dev libqrencode-dev miniupnpc libminiupnpc-dev qt5-default qt5-qmake qtbase5-dev-tools qttools5-dev-tools  build-essential libboost-dev libboost-system-dev  libboost-filesystem-dev libboost-program-options-dev libboost-thread-dev libssl-dev libdb++-dev libminiupnpc-dev  qtscript5-dev qtscript-tools  libqt5webkit5 libqt5webkit5-dev libqt5webkit5-qmlwebkitplugin
 git clone https://github.com/mammix2/global-master/ globe
 cd globe/src
 chmod 755 leveldb/build_detect_platform
